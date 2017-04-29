f6dbceda1bdc4bc15907dd74d1b9902ee25a5352
Norek <norekzal@gmail.com>
2017-04-29 21:58:46 +0200

    Add benchmarkDotNet project

---
+++ Benchmark/GraphBuilderBenchmark.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using gitWeb.Core.Features.Clone;
+using gitWeb.Core.Features.Commit;
+using gitWeb.Core.GraphBuilder;
+using LibGit2Sharp;
+using Commit = gitWeb.Core.Features.Commit.Commit;
+
+namespace Benchmark
+{
+    public class GraphBuilderBenchmark:IDisposable
+    {
+        private GraphBuilder _builder;
+        private Repository _repository;
+        private Commit[] _commits;
+
+        public GraphBuilderBenchmark()
+        {
+            _builder = new GraphBuilder();
+            _repository = new Repository("C:\\inPOS");
+            CommitProvider provider = new CommitProvider(_repository);
+            _commits = provider.GetAllFromHead().ToArray();
+        }
+
+        [Benchmark]
+        public void Run()
+        {
+            _builder.Build(_commits);
+        }
+
+        public void Dispose()
+        {
+            if (_repository != null) _repository.Dispose();
+        }
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+using gitWeb.Core.GraphBuilder;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var summary = BenchmarkRunner.Run<GraphBuilderBenchmark>();
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
